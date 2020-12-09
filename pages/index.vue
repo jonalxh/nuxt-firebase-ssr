e6feb57cff3de579088f1faf59f3a330242ec921
Jonathan Arias <jonalxh@gmail.com>
2020-12-09 12:21:10 -0500

    tests trying to implement bootstrap without success

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="container">
-    <div>
-      <h3 class="title">
+  <div>
+      <h1>
         Job list
-      </h3>
-      <ul class="text-left">
+      </h1>
+      <ul>
         <li v-for="job in jobs" :key="job.pk">
           <nuxt-link :to="{ name: 'id', params: { id: job.pk } }">
             {{ job.title }}
           </nuxt-link>
         </li>
       </ul>
-    </div>
   </div>
 </template>
 
@@ -41,35 +39,8 @@ export default {
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+<style scoped>
+h3 {
+  font-size: 50px;
 }
 </style>
